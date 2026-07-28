cfc23c9074c711cf4a6c927201391c7e6c634b52
Chamod-Kulathunga <chamodlahiru28@gmail.com>
2026-07-28 17:29:50 +0530

    Added light and dark theme configuration for the application

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -4,8 +4,45 @@ import 'app_colors.dart';
 class AppTheme {
   static ThemeData lightTheme = ThemeData(
     useMaterial3: true,
-    colorSchemeSeed: AppColors.primary,
-    scaffoldBackgroundColor: AppColors.background,
+
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: AppColors.primary,
+      brightness: Brightness.light,
+    ),
+
+    scaffoldBackgroundColor: Colors.grey.shade100,
+
     appBarTheme: const AppBarTheme(centerTitle: true, elevation: 0),
+
+    cardTheme: CardThemeData(
+      elevation: 2,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+    ),
+
+    inputDecorationTheme: InputDecorationTheme(
+      border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+    ),
+  );
+
+  static ThemeData darkTheme = ThemeData(
+    useMaterial3: true,
+
+    colorScheme: ColorScheme.fromSeed(
+      seedColor: AppColors.primary,
+      brightness: Brightness.dark,
+    ),
+
+    scaffoldBackgroundColor: const Color(0xFF121212),
+
+    appBarTheme: const AppBarTheme(centerTitle: true, elevation: 0),
+
+    cardTheme: CardThemeData(
+      elevation: 2,
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+    ),
+
+    inputDecorationTheme: InputDecorationTheme(
+      border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+    ),
   );
 }
